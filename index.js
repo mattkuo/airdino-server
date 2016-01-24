9bce300dab3a2ebdf723eea82da585c5c9304834
Matthew Kuo <matthewkuo92@gmail.com>
2016-01-24 01:21:51 -0500

    websockets should now work

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 var io = require('socket.io')();
 
+var isMobile = false,
+    isWeb = true;
+var playerMapping = {};
+var currentSockId;
+
+// Listen for player connect
 io.on('connect', function(socket) {
   console.log('client connected');
-  socket.emit('hello_world', {hello: 'world'});
 
-  socket.on('gyro', function(reading) {
-    console.log("beta: " + reading.beta + " gamma: " + reading.gamma + " alpha: " + reading.alpha);
-  });
+  if (isWeb) {
+    // Current connection is a client
+    playerMapping[socket.id] = {
+      'socket': socket,
+      'mobile': null
+    }
+    currentSockId = socket.id;
+    switchClientWeb();
+
+    // Listen for gyrometer reading
+    socket.on('gyro', function(reading) {
+      console.log("beta: " + reading.beta + " gamma: " + reading.gamma + " alpha: " + reading.alpha);
+    });
+  }
+
+  if (isMobile) {
+    // Current connection is web
+    playerMapping[currentSockId]['mobile'] = socket;
+    switchClientWeb();
+  }
+
 });
+
+function switchClientWeb() {
+  isMobile = !isMobile;
+  isWeb = !isWeb;
+}
 
 io.listen(7777);
